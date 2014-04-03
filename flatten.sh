8deb9fe1d986820e7d9698d9ffaf60510ce4645b
gotson <gotson@users.noreply.github.com>
2014-04-04 01:26:06 +0800

    Few tweaks
    Human sort files before zipping
    Preserve tags
    Force extension to cbz

--- flatten.sh
+++ flatten.sh
@@ -4,7 +4,24 @@ for file in "$@"
 do
     tmp=$(mktemp -dt tmp)
 	unar -o $tmp "$file"
+	tags=$(tag -lN "$file")
+	
+	#flatten structure
+	find $tmp -type f -print0 | xargs -0 -I%%% mv %%% $tmp
+	
+	#rename files with human sort
+	i=1
+	files=$(find $tmp -type f -not -name ".*" | gsort -V)
+	for f in $files; do
+	  new=$(printf "%04d" ${i}) #04 pad to length of 4
+	  ext=${f##*.}
+	  mv ${f} $tmp/${new}.${ext}
+	  let i=i+1
+	done
+	
 	rm "$file"
-	zip -0rj "$file" ${tmp}/*
+	new="${file%.*}.cbz"
+	zip -0rj "$new" ${tmp}/*
+	tag -s $tags "$new"
 	rm -fr $tmp
 done
