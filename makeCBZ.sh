e3eab5ab5161c682483e9c92bc93aa6ef3ba9773
gotson <gotson@users.noreply.github.com>
2014-04-08 13:17:53 +0800

    Delete makeCBZ.sh

--- makeCBZ.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pwd=`pwd`
-find "$pwd" -type d -not -empty -print0 | while IFS= read -r -d '' dir; do
-  if find "$dir" -maxdepth 1 -type f | read f; then
-    zip -0j "$pwd"/"$(basename "$dir").cbz" "$dir"/*
-  fi
-done
